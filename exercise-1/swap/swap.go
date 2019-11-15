81fa734d6a8dd59273443cd26a1c283f06047fc5
Joan López de la Franca Beltran <joanjan14@gmail.com>
2019-11-15 14:16:32 +0100

    Upload solution for exercise-1

--- exercise-1/swap/swap.go
+++ exercise-1/swap/swap.go
@@ -1,6 +1,16 @@
 package swap
 
 func Nums(nums []int) []int {
-	// TODO: Swap numbers
+	i := 0
+	j := 1
+
+	for j < len(nums) {
+		tmp := nums[i]
+		nums[i] = nums[j]
+		nums[j] = tmp
+		i+= 2
+		j+= 2
+	}
+
 	return nums
 }
